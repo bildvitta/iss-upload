20dacc665f26aa4aaa6edab5e64baeea359a7e65
SOSTheBlack <SOSTheBlack@users.noreply.github.com>
2021-05-03 14:56:58 +0000

    Fix styling

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,9 +2,9 @@
 
 namespace Bildvitta\IssUpload\Tests;
 
+use Bildvitta\IssUpload\IssUploadServiceProvider;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Orchestra\Testbench\TestCase as Orchestra;
-use Bildvitta\IssUpload\IssUploadServiceProvider;
 
 class TestCase extends Orchestra
 {
